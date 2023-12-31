7afcf0be3818e0c18721049a3fa2099d05708997
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-01 02:01:50 +0530

    implement dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,33 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/provider/ThemeProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blogging site',
-  description: 'The blogging site for developers',
-}
+  title: "Blogging site",
+  description: "The blogging site for developers",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="container">
-        <div className='wrapper'>
-      <Navbar/>
-      {children}
-      <Footer/>
-        </div>
-      </div>
-      
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
       </body>
     </html>
-  )
+  );
 }
