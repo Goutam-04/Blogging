4b4efc5b842fb9778875e7d8f5b1d36923bc9b1f
Goutam-04 <goutamnayak2804+github1@gmail.com>
2023-12-31 23:58:46 +0530

    strucerised the file system

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'Blogging site',
+  description: 'The blogging site for developers',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="container">
+        <div className='wrapper'>
+      <Navbar/>
+      {children}
+      <Footer/>
+        </div>
+      </div>
+      
+      </body>
     </html>
   )
 }
