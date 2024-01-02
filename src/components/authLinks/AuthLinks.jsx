a1ebf2627d9b3858c68a0819bd9fb5f4175a5b4b
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-02 19:27:38 +0530

    add hamburger menu

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
-import styles from "./authLinks.module.css"
-import Link from 'next/link'
+"use client";
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
 
-  const status="authenticated"
+  const status = "unauthenticated";
+
   return (
     <>
-    {(status==="notauthenticated") ?
-      (<Link href="/login" className={styles.Link}>LogIn</Link>):
-        (<div>
-
-        <Link href="/write" className={styles.Link}>Write</Link>
-          <span className={styles.Link}>LogOut</span>
-        </div>)
-    }
-
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
+        </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default AuthLinks
+export default AuthLinks;
