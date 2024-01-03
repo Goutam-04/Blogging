32197565ed1ce64f2115093c16d36c50ccf26070
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-03 22:46:55 +0530

    add categorylist secton with 6 boxes

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,10 +1,111 @@
-import React from 'react'
-import styles from "./categoryList.module.css"
+import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const categoryList = () => {
+
+const CategoryList = async () => {
+  
   return (
-    <div className={styles.container}>categoryList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+       
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+          >
+            
+              <Image
+                src="/style.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            style
+           
+          </Link>
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.food}`}
+          >
+            
+              <Image
+                src="/food.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            food
+           
+          </Link>
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+          >
+            
+              <Image
+                src="/style.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            style
+           
+          </Link>
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+          >
+            
+              <Image
+                src="/style.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            style
+           
+          </Link>
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+          >
+            
+              <Image
+                src="/style.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            style
+           
+          </Link>
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles.style}`}
+          >
+            
+              <Image
+                src="/style.png"
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            style
+           
+          </Link>
+  
+        
+      </div>
+    </div>
+  );
+};
 
-export default categoryList
+export default CategoryList;
