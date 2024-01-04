ed3227c4c9ff15263ea1816861d87d4a456da05e
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-04 20:58:40 +0530

    add /blog page

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,18 @@
+import CardList from "@/components/cardList/CardList";
+import styles from "./blogPage.module.css";
+import Menu from "@/components/Menu/Menu";
+
+const BlogPage = () => {
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}> Blog style</h1>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
