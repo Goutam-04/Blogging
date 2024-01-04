49a1c8d145a737760a414a9a4aa30aeca214e19b
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-05 01:32:16 +0530

    add a page for single blog

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,64 @@
+'use client'
+
+import Link from "next/link";
+import styles from "./comments.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+
+
+const Comments = () => {
+  const status="authenticated";
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+   
+    mutate();
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea
+            placeholder="write a comment..."
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
+        </div>
+      ) : (
+        <Link href="/login">Login to write a comment</Link>
+      )}
+      <div className={styles.comments}>
+        {/* {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item.desc}</p>
+              </div>
+            ))} */}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
