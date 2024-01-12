5baa68f860c020bdaef38a8619c107f23a44abec
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-13 01:12:04 +0530

    connect to database and fixed a major bug of connection

--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -2,6 +2,7 @@ import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./connect"
+// import { Prisma } from "@prisma/client"
 
 export const authOptions =  {
   adapter: PrismaAdapter(prisma),
