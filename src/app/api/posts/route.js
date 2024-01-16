439ba9c8fe67f2caa3bb779cefbf3efbf891269f
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-16 23:49:31 +0530

    created /api/posts api

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+
+export const GET = async()=>{
+    try {
+        const posts = await prisma.post.findMany();
+
+        return new NextResponse(
+            JSON.stringify(posts,{status:200})
+        )
+        
+    } catch (error) {
+        console.log(error);
+        return new NextResponse(
+            JSON.stringify({message:"somthing went wrong"},{status:500})
+        )
+    }
+}
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
-import Image from "next/image";
 import Card from "../card/Card";
 
+const getData = async () => {
+  const data = await fetch(
+    `http://localhost:3000/api/posts`,
+    {
+      cache: "no-store",
+    }
+  );
 
-const CardList = async ({ page, cat }) => {
+  if (!data.ok) {
+    throw new Error("Failed");
+  }
 
+  return data.json();
+};
 
+const CardList = async () => {
+
+  const posts = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        
-          <Card/>
-          <Card/>
-          <Card/>
-      
+        {posts?.map((item) => (
+          <Card item={item} key={item._id} />
+        ))}
       </div>
-    <Pagination/>
+        <Pagination/>
     </div>
   );
 };
