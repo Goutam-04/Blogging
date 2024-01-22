ca6a7cc4573e664db9f7e432d75a4db966715a08
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-22 05:48:03 +0530

    add posting functionality

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE,
+  authDomain: "blog-523d1.firebaseapp.com",
+  projectId: "blog-523d1",
+  storageBucket: "blog-523d1.appspot.com",
+  messagingSenderId: "818948713892",
+  appId: "1:818948713892:web:6b64af85fbc64063d2a531"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
