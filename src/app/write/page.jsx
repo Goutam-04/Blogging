08487c5cf77d143bd93376bd8906d70ff0d4cbc4
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-22 06:21:53 +0530

    minor change for hosting

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -13,10 +13,12 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
+import dynamic from "next/dynamic";
+
 
 const WritePage = () => {
   const { status } = useSession();
+  const ReactQuill= dynamic(()=>import ("react-quill"),{ssr:false})
   const router = useRouter();
 
   const [open, setOpen] = useState(false);
