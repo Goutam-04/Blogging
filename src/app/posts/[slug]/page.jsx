1637330514d7a09fa43cf1cbde9719530d19bbf9
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-22 06:31:50 +0530

    minor change for hosting

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,4 +1,4 @@
-import Menu from "@/components/Menu/Menu";
+import Menu from "@/components/menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
