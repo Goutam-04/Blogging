ff8258a8137799a84cbe2ec464a0182098af7569
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-01-22 05:46:18 +0530

    add posting functionality

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -2,53 +2,30 @@ import { getAuthSession } from "@/utils/authOptions";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
+// GET ALL COMMENTS OF A POST
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
 
-  const page = searchParams.get("page");
-  const cat = searchParams.get("cat");
+  const postSlug = searchParams.get("postSlug");
 
-  const POST_PER_PAGE = 2;
-
-  const query = {
-    take: POST_PER_PAGE,
-    skip: POST_PER_PAGE * (page - 1),
-    where: {
-      ...(cat && { catSlug: cat }),
-    },
-  };
-
-
-
-
-
-
-  
-  
   try {
-    const [posts, count] = await prisma.$transaction([
-      prisma.post.findMany(query),
-      prisma.post.count({ where: query.where }),
-    ]);
-    return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
+    const comments = await prisma.comment.findMany({
+      where: {
+        ...(postSlug && { postSlug }),
+      },
+      include: { user: true },
+    });
+
+    return new NextResponse(JSON.stringify(comments, { status: 200 }));
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
     );
   }
 };
 
-
-
-
-
-
-
-
-
-
-// CREATE A POST
+// CREATE A COMMENT
 export const POST = async (req) => {
   const session = await getAuthSession();
 
@@ -60,11 +37,11 @@ export const POST = async (req) => {
 
   try {
     const body = await req.json();
-    const post = await prisma.post.create({
+    const comment = await prisma.comment.create({
       data: { ...body, userEmail: session.user.email },
     });
 
-    return new NextResponse(JSON.stringify(post, { status: 200 }));
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
